11a38b4c406f5ed101082c775181f914c8a0c5b5
mmgajda <125045126+mmgajda@users.noreply.github.com>
2023-12-16 21:50:49 -0500

    Adjusting cookie banner to be responsive and work with small viewports. Closes #55

--- explorer-clothing/components/googleanalytics.tsx
+++ explorer-clothing/components/googleanalytics.tsx
@@ -53,7 +53,11 @@ const GoogleAnalytics = ({ ga_id }: { ga_id: string }) => (
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
+            
+            gtag('consent', 'default', {
+            'analytics_storage': 'denied'
+            });
+            
             gtag('config', '${ga_id}');
         `,
             }}
