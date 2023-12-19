5025df7c3676f508c882d730707abb88845013a2
mmgajda <125045126+mmgajda@users.noreply.github.com>
2023-12-19 09:38:51 -0500

    Adjusted aria labels as needed.  li/ul/ol issues stem from the way that the NextUI navbar component is configured, there is not a way to change this manually.  We must consider using a different component library for these items to fix this issue. Closes #90

--- explorer-clothing/app/page.tsx
+++ explorer-clothing/app/page.tsx
@@ -12,25 +12,25 @@ export default function LandingPage() {
                 <Hero />
             </div>
             <div className="relative text-center mt-[7rem] mb-[3rem]">
-                <h1 className='text-center'>About Explorer</h1>
+                <h1 aria-label="About Explorer" className='text-center'>About Explorer</h1>
             </div>
             <div className="relative flex">
                 <About />
             </div>
 			<div className="relative text-center mt-[7rem] mb-[3rem]">
-                <h1 className='text-center'>Find Us at the Following Retailers</h1>
+                <h1 aria-label="Find Us at the Following Retailers" className='text-center'>Find Us at the Following Retailers</h1>
 			</div>
 			<div className="relative flex left-[0.5rem]">
 				<Retailers />
 			</div>
 			<div className="relative text-center mt-[7rem] mb-[3rem]">
-                <h1 className='text-center'>Reviews</h1>
+                <h1 aria-label="Reviews" className='text-center'>Reviews</h1>
             </div>
             <div className="relative flex w-[90%]">
                 <Reviews />
             </div>
 			<div className="relative text-center mt-[3rem] mb-[3rem]">
-            	<h1>Dive Into The Season&apos;s Looks</h1>
+            	<h1 aria-label="Dive into the Season's Looks">Dive into the Season&apos;s Looks</h1>
         	</div>
             <div className="relative flex items-center mb-[12rem] left-[0.4rem]">
                 <Lookbook />
--- explorer-clothing/components/googleanalytics.tsx
+++ explorer-clothing/components/googleanalytics.tsx
@@ -1,40 +1,11 @@
-// import Script from "next/script";
-// import { pageview, GTM_ID } from '@/config/gtaghelper';
-
-// const GoogleAnalytics = ({ GTM_ID }: { GTM_ID: string }) => (
-//     <>
-//         <Script
-//             async
-//             src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
-//         ></Script>
-//         <Script
-//             id="google-analytics"
-//             dangerouslySetInnerHTML={{
-//                 __html: `
-//             window.dataLayer = window.dataLayer || [];
-//             function gtag(){dataLayer.push(arguments);}
-//             gtag('js', new Date());
-            
-//             gtag('consent', 'default', {
-//             'analytics_storage': 'denied'
-//             });
-            
-//             gtag('config', '${GTM_ID}');
-//         `,
-//             }}
-//         ></Script>
-//     </>
-// );
-// export default GoogleAnalytics;
-
+import { gtm_id } from "@/config/gtaghelper";
 import Script from "next/script";
-import { pageview, ga_id } from '@/config/gtaghelper';
 
 const GoogleAnalytics = ({ ga_id }: { ga_id: string }) => (
     <>
         <Script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-XS3EW9ZRZL`}
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtm_id}`}
         ></Script>
         <Script
             id="google-analytics"
@@ -44,7 +15,7 @@ const GoogleAnalytics = ({ ga_id }: { ga_id: string }) => (
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', '${ga_id}');
+            gtag('config', '${gtm_id}');
         `,
             }}
         ></Script>
