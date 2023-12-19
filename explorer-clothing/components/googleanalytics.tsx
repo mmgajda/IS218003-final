6ef8064904b07dbbd5d16d22f179805c4fe4962a
mmgajda <125045126+mmgajda@users.noreply.github.com>
2023-12-18 21:17:37 -0500

    All tests passed.

--- explorer-clothing/components/googleanalytics.tsx
+++ explorer-clothing/components/googleanalytics.tsx
@@ -1,50 +1,10 @@
-// 'use client';
-
-// import { usePathname, useSearchParams } from 'next/navigation';
-// import { useEffect } from 'react';
-// import { pageview } from '@/config/gtaghelper';
-
-// export default function GoogleAnalytics({GA_MEASUREMENT_ID} : {GA_MEASUREMENT_ID : string}){
-//     const pathname = usePathname()
-//     const searchParams = useSearchParams()
-
-//     useEffect(() => {
-//         const url = pathname + searchParams.toString()
-
-//         pageview(GA_MEASUREMENT_ID, url);
-
-//     }, [pathname, searchParams, GA_MEASUREMENT_ID]);
-
-//     return (
-//         <>
-//             <Script strategy="afterInteractive" 
-//                 src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}/>
-//             <Script id='google-analytics' strategy="afterInteractive"
-//                 dangerouslySetInnerHTML={{
-//                 __html: `
-//                 window.dataLayer = window.dataLayer || [];
-//                 function gtag(){dataLayer.push(arguments);}
-//                 gtag('js', new Date());
-
-//                 gtag('consent', 'default', {
-//                     'analytics_storage': 'denied'
-//                 });
-
-//                 gtag('config', '${GA_MEASUREMENT_ID}', {
-//                     page_path: window.location.pathname,
-//                 });
-//                 `,
-//                 }}
-//             />
-//         </>
-// )}
 import Script from "next/script";
 
 const GoogleAnalytics = ({ ga_id }: { ga_id: string }) => (
     <>
         <Script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-P6P117GN3H`}
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
         ></Script>
         <Script
             id="google-analytics"
--- explorer-clothing/components/notification.tsx
+++ explorer-clothing/components/notification.tsx
@@ -9,8 +9,10 @@ const Notification: React.FC<NotificationProps> = ({ message, type }) => {
     if (!message) return null;
 
     return (
-        <div className={`notification ${type}`} data-testid={`notification-${type}`}>
+        <div className= {`notification ${type}`} data-testid={`notification-${type}`}>
+            <div className='text-black'>
             {message}
+            </div>
         </div>
     );
 };
