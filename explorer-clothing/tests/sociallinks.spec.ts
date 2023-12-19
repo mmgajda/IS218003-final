2ca220603a1c39a9225da8a2178e355e19e081db
mmgajda <125045126+mmgajda@users.noreply.github.com>
2023-12-19 15:33:44 -0500

    Vercel Deployment

---
+++ explorer-clothing/tests/sociallinks.spec.ts
@@ -0,0 +1,54 @@
+// import { browser } from "process";
+
+// const { test, expect } = require('@playwright/test');
+
+// test('Socials Link Test', async ({ page }) => {
+//     await page.goto('http://localhost:3000/');
+
+
+//     //Comment out two of the three tests and run on each link, one at a time.
+//     //instagram test
+//     const instaLink = page.locator('#instagram');
+
+//     // await expect(instaLink).toBeVisible();
+
+//     // // Step 4: Verify the href attribute of the link
+//     // const href = await instaLink.getAttribute('href');
+//     // expect(href).toBe('https://instagram.com');
+
+//     const [newPage] = await Promise.all([
+//         page.waitForEvent('popup'),
+//         instaLink.click()
+//     ]);
+
+//     // Step 4: Verify the URL of the new page
+//     await expect(newPage).toHaveURL('https://www.instagram.com');
+
+//     await newPage.close();
+
+
+//     //twitter test
+//     const twitterLink = page.locator('#twitter');
+
+//     const [newPage] = await Promise.all([
+//         page.waitForEvent('popup'),
+//         instaLink.click()
+//     ]);
+//     await expect(newPage).toHaveURL('https://www.twitter.com');
+
+//     await newPage.close();
+
+
+//     //tiktok test
+//     const tiktokLink = page.locator('#tiktok');
+
+//     const [newPage] = await Promise.all([
+//         page.waitForEvent('popup'),
+//         instaLink.click()
+//     ]);
+
+//     // Step 4: Verify the URL of the new page
+//     await expect(newPage).toHaveURL('https://www.tiktok.com');
+
+//     await newPage.close();
+// });
