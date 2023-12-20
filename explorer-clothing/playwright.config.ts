9d614f86598ffb1fc86655237eb2ca168703da9b
mmgajda <125045126+mmgajda@users.noreply.github.com>
2023-12-20 15:28:04 -0500

    Further Playwright testing via Vercel

--- explorer-clothing/playwright.config.ts
+++ explorer-clothing/playwright.config.ts
@@ -53,16 +53,16 @@ export default defineConfig({
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] },
     },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
 
     /* Test against branded browsers. */
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
     {
       name: 'Google Chrome',
       use: { ...devices['Desktop Chrome'], channel: 'chrome' },
